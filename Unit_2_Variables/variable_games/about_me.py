a80a1dfa37591dc491e1b2753fb6ffba54fec05d
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-06 15:39:38 -0800

    Create about_me.py

---
+++ Unit_2_Variables/variable_games/about_me.py
@@ -0,0 +1,14 @@
+name = "Jack" #strimg
+age = 26 #integer
+has_job = True #boolean
+height = 6.2 #float
+
+#To add a string to other data types use + and concantenation
+#To concantenate, wrap the variable () and put str in front of it
+
+print("My name is " + name)
+print("My unofficial age is " + str(age))
+print("The fact that I have a job is " + str(has_job))
+print("My height in feet is " + str(height))
+
+
