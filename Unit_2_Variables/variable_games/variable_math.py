9fd062bf5425b5e70aeaa0f3a45d9fc0b337e3fd
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-06 17:40:31 -0800

    Create variable_math.py

---
+++ Unit_2_Variables/variable_games/variable_math.py
@@ -0,0 +1,26 @@
+#What you will learn
+#   - Basic operations in python
+#   - Variable math
+#   - Multiplication of strings
+
+
+#Four main mathematical operations
+#Addition, subtraction, division, multiplication
+# + , - , *, /
+#They can work with variables!!
+
+num1 = 5
+num2 = 2
+
+print(num1 + num2) # addition
+print(num1 - num2) # subtraction
+print(num1 * num2) # multiplication
+print(num1 / num2) # division
+
+#Only muliplication works for a string and an integer
+#If its addition, you have to type cast the integer into a string
+
+word = "high"
+
+print(word + str(num1)) # Concantenation (We did this!)
+print(word * num1) #String multiplication
