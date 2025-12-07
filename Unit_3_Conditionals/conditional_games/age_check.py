29209cbb4877039798e199c554c72a7218932ff5
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-07 15:39:25 -0800

    Create age_check.py

---
+++ Unit_3_Conditionals/conditional_games/age_check.py
@@ -0,0 +1,26 @@
+# if - else statement
+# IF a condition is True do something
+# Else do something else
+
+
+# Comparison Operators - Very important!
+
+# == -> is equal to
+# > -> is greater than
+# < -> is less than
+# >= -> is greater than or equal to
+# <= -> is less than or equal to
+# != -> not equal to
+
+# == vs. =
+# ==  is a comparison operator, meaning it asks if something equals something else
+# = is an assignment operator, meaning it sets something equal to something else
+
+#Variables
+age = int(input("Enter age: "))
+
+if age >= 18:
+    print("You are an adult!")
+else:
+    print("You are not an adult!")
+          
