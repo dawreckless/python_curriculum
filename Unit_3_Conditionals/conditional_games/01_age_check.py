42e3aebc43f86bb736df085bd7a58076b49b254a
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-07 15:44:58 -0800

    Update and rename age_check.py to 01_age_check.py

--- Unit_3_Conditionals/conditional_games/01_age_check.py
+++ Unit_3_Conditionals/conditional_games/01_age_check.py
@@ -19,8 +19,14 @@
 #Variables
 age = int(input("Enter age: "))
 
-if age >= 18:
+if age >= 65: 
+    print("You are a senior")
+elif age >= 18: # Else if statements run if the if statement is false
     print("You are an adult!")
+elif age < 0:
+    print("How are you typing this? You are not born yet!")
 else:
     print("You are not an adult!")
+
+#Always put the LEAST specific condition first
           
