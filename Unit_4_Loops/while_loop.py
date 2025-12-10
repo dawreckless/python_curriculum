cbc20f7c61f11a0581933fcc304bb3e86a0fb2b6
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-09 17:21:49 -0800

    Create while_loops.py

---
+++ Unit_4_Loops/while_loop.py
@@ -0,0 +1,36 @@
+# While Loops - Do something WHILE a condition is TRUE
+
+# while condition is true:
+#     do something
+
+#If the condition is false, the while loop will stop
+
+number = 1
+while number < 3:
+  print(number)
+  number = number + 1
+
+#What this is doing
+
+# The while loop will keep looping as long as number is less than 3
+# Every time the loop runs, number is increased by one
+
+#First loop:
+
+# number = 1
+#1 < 3 → True → loop runs
+#prints: 1
+#number becomes 2
+
+#Second loop:
+
+#number = 2
+#2 < 3 → True → loop runs
+#prints: 2
+#number becomes 3
+
+#Third loop:
+
+#number = 3
+#3 < 3 → False → loop stops
+#Program ends
