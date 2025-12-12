9a853dcd1e3e11866ca22cda20bf27040122439b
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-11 22:21:30 -0800

    Create for_loop_break.py

---
+++ Unit_4_Loops/loop_games/for_loop_break.py
@@ -0,0 +1,11 @@
+# What you will learn:
+#   - Review of for loops
+
+# This loop stops early when i reaches 5
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+print("Loop stopped because i reached 5.")
