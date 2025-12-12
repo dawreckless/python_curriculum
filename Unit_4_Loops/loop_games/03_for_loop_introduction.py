8f34373a603eb587351a74a0874b5091bb9da945
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-11 22:12:54 -0800

    Create 03_for_loop_introduction.py

---
+++ Unit_4_Loops/loop_games/03_for_loop_introduction.py
@@ -0,0 +1,21 @@
+#what you will learn
+#   - For loops
+#   - Start, Stop, Step
+
+# A for loop repeats something a set number of times.
+
+# "i" is just a counter. It changes each time the loop runs.
+# On each loop:
+#   i = 0
+#   i = 1
+#   i = 2
+#   i = 3
+#   i = 4
+
+# "range(5)" means: do it 5 times (0, 1, 2, 3, 4).
+
+for i in range(5):
+  print(i)
+
+
+
