0b29f74fdf3f7d0badc34c9728ae8e561563e0a3
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-11 22:22:36 -0800

    Create 06_for_loop_continue.py

---
+++ Unit_4_Loops/loop_games/06_for_loop_continue.py
@@ -0,0 +1,11 @@
+# What you will learn:
+#   - Review of for loops
+
+# This loop skips the number 5
+
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
+
+print("The number 5 was skipped.")
