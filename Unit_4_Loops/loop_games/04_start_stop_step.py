4737b506db7ac0719985a1c59c2fde155a39dbba
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-11 22:16:57 -0800

    Create 04_start_stop_step.py

---
+++ Unit_4_Loops/loop_games/04_start_stop_step.py
@@ -0,0 +1,24 @@
+# What you will learn :
+#   - How range() really works
+#   - Start, Stop, Step
+
+# range(stop)        → counts from 0 up to stop - 1
+# range(start, stop) → counts from start up to stop - 1
+# range(start, stop, step) → counts by step
+
+# Examples:
+
+# 1. Start at 0, stop at 5
+for i in range(5):
+    print(i)
+
+# 2. Start at 3, stop at 8
+for i in range(3, 8):
+    print(i)
+
+# 3. Count by 2
+for i in range(0, 10, 2):
+    print(i)
+#4. Predict what this will output
+for i in range(2, 8, 4):
+  print(i)
