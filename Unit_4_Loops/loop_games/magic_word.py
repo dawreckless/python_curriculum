98201bad57ea89de016e57698fae99822c4c4fb4
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-11 22:39:13 -0800

    Create magic_word.py

---
+++ Unit_4_Loops/loop_games/magic_word.py
@@ -0,0 +1,18 @@
+magic_word = "open"
+
+while True:
+    word = input("Say the magic word: ")
+
+    if word == "":
+        print("You didn't say anything!")
+        continue
+
+    if word == "quit":
+        print("Game ended.")
+        break
+
+    if word == magic_word:
+        print("Gate opened!")
+        break
+    else:
+        print("Wrong!")
