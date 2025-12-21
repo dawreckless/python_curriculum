5fb14a0de34aa81eb1efeb04417c443a505e9240
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-21 14:04:26 -0800

    Update and rename 02_age_calculator.py to 02_printing_combine_concantenate.py

---
+++ Unit_2_Variables/variable_games/02_printing_combine_concantenate.py
@@ -0,0 +1,39 @@
+# What you will learn
+#   - Review of Making Variables
+#   - Review of data types
+#   - Printing
+#   - Concantenation
+
+# What is printing?
+
+# Printing displays output to the console
+
+print("Hello World")
+
+# To print, type print() and inside the parenthesis, type what you would like to output
+# "Hello World" is a string, because it has quotes
+
+# Adding Variables
+
+number = 10
+print(number + number)
+# Outputs 20
+
+string_number = "10"
+print(string_number + string_number)
+# Outputs "1010"
+
+#Concantention
+
+#The process of ADDING two or more STRINGS together
+name = "Aaron"
+age = 14
+print("My name is " + name + ". My age is " + str(age) + ".")
+
+
+
+
+
+
+
+
