9c9b0f429f8e13cd289b1a820a0ec23e80782c8e
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-21 13:22:55 -0800

    Update and rename 01_about_me.py to 01_intro_to_variables.py

---
+++ Unit_2_Variables/variable_games/01_intro_to_variables.py
@@ -0,0 +1,24 @@
+# What you will learn
+#  - Making Variables
+#  - Data types
+
+# Making Variables:
+# name of variable = value
+
+# Example:
+# age = 14
+
+# The 4 main data types
+name = "Aaron" # String 
+   # Strings use quotation marks and represent text
+age = 14 #Integer
+   # Integers are whole number values
+height = 5.625 #Float
+   # Floats are numbers with a decimal point
+is_male = True # Boolean
+   # Booleans are either True or False (capitalized)
+
+
+
+
+
