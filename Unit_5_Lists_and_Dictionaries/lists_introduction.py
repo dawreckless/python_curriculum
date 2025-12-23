a2d1932e1bc78d756a75925d99462d1f5fac7885
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-23 12:28:33 -0800

    Create lists_introduction.py

---
+++ Unit_5_Lists_and_Dictionaries/lists_introduction.py
@@ -0,0 +1,17 @@
+# What you will learn
+#   - Introduction to lists
+#   - Indexing
+
+# Lists use square brackets and commas
+subjects = ["math","language arts","history","science"]
+# This is called indexing.
+# To index, put the list name and square brackets, then the position of the value.
+# The first value is always 0.
+print(subjects[0]) # prints "math"
+
+print("I am going to say every class!")
+
+# For every item in the list, print each item.
+
+for i in subjects:
+  print(i)
