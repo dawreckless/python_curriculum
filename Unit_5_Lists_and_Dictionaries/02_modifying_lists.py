484f6c200357742b89cb98158df3f495d83019b7
dawreckless <lee.aaronsamuel@gmail.com>
2025-12-23 12:51:31 -0800

    Update 02_modifying_lists.py

--- Unit_5_Lists_and_Dictionaries/02_modifying_lists.py
+++ Unit_5_Lists_and_Dictionaries/02_modifying_lists.py
@@ -1,1 +1,27 @@
-##
+# What you will learn
+#   - Review of Introduction to Lists
+#   - Review of Indexing
+#   - Adding and Removing Items in the List
+#   - Renaming Items in the List
+
+animals = ["dog","cat","bird","fish"]
+print(animals)
+
+# Append adds a value to the end of a list
+animals.append("hamster")
+print(animals)
+print("I added a value to my list using append!")
+
+# Removes a specified item from a list
+animals.remove("bird")
+print(animals)
+print("I removed a value in my list using remove!")
+
+# You can set a value in the list to a different value like this:
+animals[0] = "cow"
+print(animals)
+print("I set a value in my list to a different value!")
+
+
+
+
